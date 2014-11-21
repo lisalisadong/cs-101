10051b3dfc79bf9437c73f3325b37b21e369b6d0
Qingxiao Dong <qingxiao.dong@gmail.com>
2014-11-21 00:52:55 -0500

    Nit.

--- lesson_6/quiz/fasterFibonacci.py
+++ lesson_6/quiz/fasterFibonacci.py
@@ -5,8 +5,8 @@ def fibonacci(n):
     i = 0
     result0 = 0
     result1 = 1
-    while i < n:                    #for i in range(0, n):
-        cache = result1             #result0, result1 = result1, result0 + result1
+    while i < n:
+        cache = result1
         result1 = result0 + result1
         result0 = cache
         i = i + 1
