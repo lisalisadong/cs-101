9e92d9444e407a217f2672f8ae590a44f9d4eb2b
Qingxiao Dong <qingxiao.dong@gmail.com>
2014-12-05 00:25:26 -0500

    Made up lesson 2.5.

---
+++ 03_lesson_2.5/quiz/defineDaysBetweenDates.py
@@ -0,0 +1,47 @@
+# Define a daysBetweenDates procedure that would produce the
+# correct output if there was a correct nextDay procedure.
+#
+# Note that this will NOT produce correct ouptuts yet, since
+# our nextDay procedure assumes all months have 30 days
+# (hence a year is 360 days, instead of 365).
+# 
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar, and the first date is not after
+       the second."""
+        
+    # YOUR CODE HERE!
+    daysBetweenDates = 0
+    while True:
+        if (year1, month1, day1) != (year2, month2, day2):
+            daysBetweenDates += 1
+            year1, month1, day1 = nextDay(year1, month1, day1)
+        else:
+            break
+    return daysBetweenDates
+
+def test():
+    test_cases = [((2012,9,30,2012,10,30),30), 
+                  ((2012,1,1,2013,1,1),360),
+                  ((2012,9,1,2012,9,4),3)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
---
+++ 03_lesson_2.5/quiz/defineNextDay.py
@@ -0,0 +1,21 @@
+###
+### Define a simple nextDay procedure, that assumes
+### every month has 30 days.
+###
+### For example:
+###    nextDay(1999, 12, 30) => (2000, 1, 1)
+###    nextDay(2013, 1, 30) => (2013, 2, 1)
+###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
+###
+
+def nextDay(year, month, day):
+    """
+    Returns the year, month, day of the next day.
+    Simple version: assume every month has 30 days.
+    """
+    # YOUR CODE HERE
+    if day < 30:
+        return year, month, day + 1
+    if month < 12:
+        return year, month + 1, 1
+    return year + 1, 1, 1
